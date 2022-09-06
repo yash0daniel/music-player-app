7bd1916a57c61e816f186fb72aa2bbec15a0b1e6
yash0daniel <yashmogannavar99@gmail.com>
2022-09-07 05:04:48 +0530

    basic modifications

--- src/angular.material.module.ts
+++ src/angular.material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button'; 
 
 @NgModule({
     imports: [
         MatCardModule,
         MatIconModule, 
         MatProgressBarModule,
+        MatButtonModule,
     ],
     exports: [
         MatCardModule,
         MatIconModule,
         MatProgressBarModule,
+        MatButtonModule,
     ]
 })
 
